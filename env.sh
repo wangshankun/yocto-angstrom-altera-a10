f2c5c5c30c9a7156771c8eb5d33b6a19f3a662b6
shankun <wangshankun2011@hotmail.com>
2017-10-12 14:02:07 +0800

    update kernel to stable release

--- env.sh
+++ env.sh
@@ -3,8 +3,10 @@ MPWD=$(pwd)
 export PATH=$PATH:$MPWD
 MACHINE=arria10 . ./setup-environment
 sed -i '/meta-altera/a \ \ ${TOPDIR}\/sources\/meta-altera-refdes \\' conf/bblayers.conf
-export KERNEL_PROVIDER=linux-altera-ltsi
-export KERNEL_TAG=refs/tags/ACDS16.1_REL_GSRD_PR
+#export KERNEL_PROVIDER=linux-altera-ltsi
+export KERNEL_PROVIDER=linux-altera
+#export KERNEL_TAG=refs/tags/ACDS16.1_REL_GSRD_PR
+export KERNEL_TAG=refs/tags/rel_socfpga-4.4_16.05.01_pr
 export UBOOT_TAG=refs/tags/ACDS16.1_REL_GSRD_PR
 BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE KERNEL_TAG UBOOT_TAG KERNEL_PROVIDER"
 
